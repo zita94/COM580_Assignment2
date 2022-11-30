8855489478bd1ad3679752a264879dc727f5b1e3
Zita Koczka <koczka-z@ulster.ac.uk>
2022-11-30 20:09:47 +0000

    added student repository

---
+++ Students/Students/Models/Repositories/IStudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Students.Models.Repositories
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<Student> GetAllStudents();
+        Student SelectByID (int id);
+        void Insert (Student student);
+        void Update (Student student);
+        void Delete (int id);
+        void Save();
+    }
+}
---
+++ Students/Students/Models/Repositories/StudentRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Students.Models.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private StudentDBContext db = null;
+
+        public StudentRepository() 
+        {
+            this.db = new StudentDBContext();
+        }
+
+        public StudentRepository(StudentDBContext db)
+        {
+            this.db = db;
+        }
+
+        public void Delete(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return db.Students.OrderBy(x => x.Surname).ToList();
+        }
+
+        public void Insert(Student student)
+        {
+            db.Students.Add(student);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public Student SelectByID(int id)
+        {
+            return db.Students.Find(id);
+        }
+
+        public void Update(Student student)
+        {
+            db.Entry(student).State = System.Data.Entity.EntityState.Modified;
+        }
+    }
+}
