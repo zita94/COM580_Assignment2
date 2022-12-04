d498665df63a2e4de5b532c2a563b075f3b5017f
Zita Koczka <koczka-z@ulster.ac.uk>
2022-12-04 01:43:39 +0000

    finished validation, edited about and contact page

--- Students/Students/Controllers/HomeController.cs
+++ Students/Students/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Students.Controllers
 {
@@ -15,14 +11,15 @@ namespace Students.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.AboutMessage = "This web application is for creating, editing, deleting and listing details of " +
+                "international students with the information stored in a supporting database.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.ContactMessage = "Created by:";
 
             return View();
         }
--- Students/Students/Student.cs
+++ Students/Students/Student.cs
@@ -7,22 +7,28 @@ namespace Students
         public int Id { get; set; }
 
         [Required]
-        [StringLength(40)]
+        [StringLength(40, ErrorMessage = "Please enter a surname that is less than 40 characters")]
         public string Surname { get; set; }
 
         [Required]
-        [StringLength(30)]
+        [Display(Name = "First Name")]
+        [StringLength(30, ErrorMessage = "Please enter a first name that is less than 30 characters")]
         public string FirstName { get; set; }
 
         [Required]
-        [StringLength(15)]
+        [Display(Name = "Telephone Number")]
+        [StringLength(15, ErrorMessage = "Please enter a phone number that is less than 15 numbers")]
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
+
         public string TelephoneNo { get; set; }
 
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "Please enter an email address that is less than 50 characters")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "Please enter a country that is less than 50 characters")]
+        [RegularExpression("[a-zA-Z]+", ErrorMessage = "Please enter a valid country")]
         public string Country { get; set; }
     }
 }
